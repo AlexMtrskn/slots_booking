617347d5ee85151843db559d6ccacdc3eb572aca
AlexMtrskn <alexmitryashkin@gmail.com>
2025-06-30 16:16:01 +0800

    Update booking.py

--- src/booking.py
+++ src/booking.py
@@ -124,11 +124,9 @@ def selenium_request(usr):
     with SB(uc=True, xvfb=True) as sb:
         print('SB started')
         try:
-            sb.uc_open_with_reconnect(website_login, 4)
-            sb.set_window_size(1920, 1080)
+            sb.driver.uc_open_with_reconnect(website_login, 6)
             sb.sleep(2)
             # /// sb.save_screenshot(str(datetime.datetime.now()),folder='screenshots')
-            sb.uc_gui_handle_cf()
             sb.uc_gui_handle_cf()
             #sb.uc_gui_handle_captcha()
             #sb.uc_gui_click_captcha()
